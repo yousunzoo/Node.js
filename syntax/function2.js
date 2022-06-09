9289a1621f074d72a398da125e0c0ac2c8baaa63
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-09 10:11:56 +0900

    25강) 함수

---
+++ syntax/function2.js
@@ -0,0 +1,11 @@
+// round : 반올림
+console.log(Math.round(1.9)); // 2
+console.log(Math.round(1.2)); // 1
+
+function sum(first, second){ // parameter
+  return first+second;
+}
+
+console.log(sum(2,4)); //argument
+
+Math.round(1.9);
