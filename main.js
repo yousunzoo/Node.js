a6cc81224465dbe08ec43880aa83e87b2ad6c731
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-09 11:41:35 +0900

    37강) 글 삭제(삭제 버튼 구현)

--- main.js
+++ main.js
@@ -55,7 +55,14 @@ var app = http.createServer(function (request, response) {
         fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `
+          <a href="/create">create</a>
+          <a href="/update?id=${title}">update</a>
+          <form action="delete_process" method="POST">
+          <input type="hidden" name="id" value="${title}">
+          <input type="submit" value="delete">
+          </form>
+          `);
           response.writeHead(200);
           response.end(template);
         });
