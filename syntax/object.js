6a28cd919146ee0bc628f88564af4d47af12d7b9
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-10 14:35:12 +0900

    40강) 객체의 반복

--- syntax/object.js
+++ syntax/object.js
@@ -1,5 +1,11 @@
 var members = ['seon', 'k8805', 'hoya'];
 console.log(members[1]);
+var i = 0;
+
+while(i<members.length){
+  console.log('array loop', members[i]);
+  i = i + 1;
+}
 
 var roles = {
   'programmer':'seon',
@@ -7,4 +13,10 @@ var roles = {
   'con':'hoya'
 }
 
-console.log(roles.programmer)
+console.log(roles.programmer);
+console.log(roles['programmer']);
+
+
+for(var name in roles){
+  console.log('object ==> ', name, 'value ==> ', roles[name]);
+}
