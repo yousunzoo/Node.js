6365a1137e874035151852f0b1a0997f77d1ddbf
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-10 14:31:15 +0900

    39강) 객체의 형식

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['seon', 'k8805', 'hoya'];
+console.log(members[1]);
+
+var roles = {
+  'programmer':'seon',
+  'designer':'k8805',
+  'con':'hoya'
+}
+
+console.log(roles.programmer)
